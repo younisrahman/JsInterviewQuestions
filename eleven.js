339108c593ad8d0fa0c48a6977f005956506cc0f
Younis Rahman <younisrahman007@gmail.com>
2022-02-24 22:38:18 +0600

    twelve

---
+++ eleven.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, '2'));
+// A : "12"
+// B: 12
+//C: 102
+// D: "102"
---
+++ twelve.js
@@ -0,0 +1,5 @@
+console.log(typeof []);
+// A object
+// B array
+// c object
+// D Array
