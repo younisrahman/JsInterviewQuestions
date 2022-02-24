385055219bf77836f3965f26b1f89bfcf5695419
Younis Rahman <younisrahman007@gmail.com>
2022-02-25 01:05:16 +0600

    twenty four

---
+++ twentyfour.js
@@ -0,0 +1,10 @@
+const obj = {
+  a: 'one',
+  b: 'two',
+  a: 'three',
+};
+console.log(obj);
+// A: { a: "one", b:"two" }
+// B: {b: "two",a: "three" }
+// C: { a: "three", b: "two" }
+// D: SyntaxError
