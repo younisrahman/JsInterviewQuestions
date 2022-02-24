eb901081face81ccc2d773fe2522d8fa60c78dce
Younis Rahman <younisrahman007@gmail.com>
2022-02-24 21:02:51 +0600

    nine

---
+++ nine.js
@@ -0,0 +1,9 @@
+let a = 3;
+let b = new Number(3);
+console.log(a == b);
+console.log(a === b);
+// console.log(typeof a);
+// console.log(typeof b);
+// A : true and false
+// B: true and true
+// C: false and false
